7daa67d992da09c73c66246ec8b54c3fbae4912c
Guillaume Fabbi <guigui.fabbi@gmail.com>
2025-02-21 14:01:03 +0100

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://nimble-salmiakki-89a2ad.netlify.app/"
+  site: "https://nimble-salmiakki-89a2ad.netlify.app/",
+  integrations: [preact()]
 });
