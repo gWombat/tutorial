57e988d4687ec826d4f4a6ce8f508b4e1b73ec9e
Guillaume Fabbi <guigui.fabbi@gmail.com>
2025-02-21 13:52:19 +0100

    Add RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://nimble-salmiakki-89a2ad.netlify.app/"
+});
